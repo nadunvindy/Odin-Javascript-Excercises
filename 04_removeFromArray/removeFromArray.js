2c570ab10a01d5bab8a5b83ae1f72215ba30068b
Nadun Vindiya <nadunbalasuriya03@gmail.com>
2022-12-11 23:42:35 +0800

    Complete removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(numArray,...nums) {
+   let arrayNum = numArray;
+   for (let num of nums){
+   let index = numArray.indexOf(num);
+   if (index!=-1){
+   numArray.splice(index,1);}
+}
+   return numArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
