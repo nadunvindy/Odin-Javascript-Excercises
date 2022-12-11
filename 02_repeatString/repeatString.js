23f0d958272a0224b4d4c5498df5cb3fa9845f77
Nadun Vindiya <nadunbalasuriya03@gmail.com>
2022-12-11 22:33:44 +0800

    Complete stringRepeat excercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
-};
+    const repeatString = function(string,num) {
+        let finalString = "";
+        if (num<0) {
+            return "ERROR"
+            
+        }
+        else{
+        for (let i =0;i<num;i++){
+            finalString += string;
+        }
+        return finalString;
+    }
+    }
 
 // Do not edit below this line
 module.exports = repeatString;
