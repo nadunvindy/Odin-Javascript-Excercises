eae22926027d5973c4ab888b538e1a6e95d671c5
Nadun Vindiya <nadunbalasuriya03@gmail.com>
2022-12-13 18:12:41 +0800

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+var regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
 
+function removePunctuation(string) {
+  return string.replace(regex, '');
+}
+
+
+const palindromes = function (string) {
+    let nospacestring = (string.replace(/\s+/g, '')).toLowerCase();
+    nospacestring.replace(/[^\w\s\']|_/g, "");
+    let nopunct = removePunctuation(nospacestring);
+    let reversed = nopunct.split('').reverse().join('');
+    if (nopunct==reversed){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
