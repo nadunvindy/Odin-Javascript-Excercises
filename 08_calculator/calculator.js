6e51bf46159296173d6a6e2bf97de6d04745a97e
Nadun Vindiya <nadunbalasuriya03@gmail.com>
2022-12-13 17:34:32 +0800

    Complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	let result = 0;
+  for(let i =0;i<nums.length;i++){
+    result += nums[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let result = 1;
+  for(let i =0;i<nums.length;i++){
+    result *= nums[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+
+const power = function(num,pow) {
+	return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num==0) {return 1;}
+	let fact = num;
+  for(let i =1;i<num;i++){
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
