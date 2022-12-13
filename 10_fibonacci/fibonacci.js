f9f4d19c454017652c818591259af036ba4eabc9
Nadun Vindiya <nadunbalasuriya03@gmail.com>
2022-12-13 19:02:47 +0800

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (Number(num)<0){return "OOPS"};
+        let fibsequence=[];
+        for(let i=0;i<Number(num)+1;i++){
+            if(i==0){
+                fibsequence.push(0)
+            }
+            else if(i==1){
+                fibsequence.push(1)
+            }
+            else{
+                fibsequence.push(fibsequence[i-1]+fibsequence[i-2]);
+            }  
+        }
+        return fibsequence[Number(num)];
+        
+    
+    
 };
 
 // Do not edit below this line
