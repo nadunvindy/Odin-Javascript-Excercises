7aace1b02ca1fc17fe147457aaa4b51a84090b3d
Nadun Vindiya <nadunbalasuriya03@gmail.com>
2022-12-13 19:50:18 +0800

    Complete getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let authors = [];
+    for (let i =0;i<books.length;i++){
+        authors.push(books[i].title);
+    }
+    return authors;
 };
 
 // Do not edit below this line
